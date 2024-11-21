7999475dfbc78ede0a0fc2572364ebe64e6844f4
Anastasia <n05012007@gmail.com>
2024-11-21 22:02:47 +0100

    update all

--- js/task-2.js
+++ js/task-2.js
@@ -1,15 +1,3 @@
-function makeArray(firstArray, secondArray, maxLength) {
-const combinedArray = firstArray.concat(secondArray);
-if (combinedArray.length > maxLength && Array.isArray(combinedArray)) {
-    return combinedArray.slice(0,maxLength);
-} else {
-    return combinedArray;
+const calcAverageCalories = function(days) {
+    
 }
-}
-
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2));
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
