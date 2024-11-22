9318233e4859c0a41434958d18a915db718889cb
Anastasia <n05012007@gmail.com>
2024-11-22 21:56:52 +0100

    update all

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,25 @@
-function filterArray(numbers, value) {
-  const massiv = [];
-  for(const i of numbers) {
-    if(i > value) {
-       massiv.push(i);
-    }
-  }
-  return massiv;
+const profile = {
+    username: "Jacob",
+  playTime: 300,
+changeUsername(newName) {
+  profile.username = newName;
+},
+updatePlayTime(hours) {
+profile.playTime += hours;
+},
+getInfo() {
+  return `${profile.username} has ${profile.playTime} active hours!`
 }
+};
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([1, 2, 3, 4, 5], 4));
-console.log(filterArray([1, 2, 3, 4, 5], 5));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
